115eddbc96278200ef43e9de4c07232ecc00970f
Hen2609 <chen.zilberman@gmail.com>
2020-08-25 12:08:58 +0300

    step1 Completed

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <p>Awesome Todo App</p>
+    <table>
+      <tr>
+        <th>תיאור</th>
+        <th>בצע</th>
+      </tr>
+      <tr v-for="(todo,index) in todos" :key="index">
+        <td>{{todo}}</td>
+        <td>
+          <button>תלחץ אותי</button>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   methods: {},
+  data() {
+    return {
+      todos: ["daniel", "sabag", "wasHere"],
+    };
+  },
 };
 </script>
 
